344b6ee1b47e416367cda3fa696979590f9eef20
GusGD <si_gustavo@outlook.com>
2025-10-07 15:17:41 -0300

    refactor: remover dependência circular com WaitTimeModule
    - Remover import forwardRef desnecessário
    - WaitTimeService será integrado posteriormente
    - Usar valor padrão fixo de 15min por enquanto

--- backend/src/presentation/controllers/consultations/consultations.module.ts
+++ backend/src/presentation/controllers/consultations/consultations.module.ts
@@ -1,11 +1,9 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConsultationsController } from './consultations.controller';
 import { ConsultationsService } from '@/application/services/consultations.service';
 import { SpecialtyFilterService } from '@/shared/services/specialty-filter.service';
-import { WaitTimeModule } from '../wait-time/wait-time.module';
 
 @Module({
-  imports: [forwardRef(() => WaitTimeModule)],
   controllers: [ConsultationsController],
   providers: [ConsultationsService, SpecialtyFilterService],
   exports: [ConsultationsService, SpecialtyFilterService],
